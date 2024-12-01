4a83fb65c39cf24153356482bfb3c24ebb9171a4
md-suhag <abdussalamsuhag104@gmail.com>
2024-12-01 19:04:30 +0600

    added validation and user related controller

--- routes/chat.route.js
+++ routes/chat.route.js
@@ -14,27 +14,48 @@ import {
   sendAttachments,
 } from "../controllers/chat.controller.js";
 import { attachmentMulter } from "../middlewares/multer.js";
+import {
+  addMemberValidator,
+  chatIdValidator,
+  newGroupValidator,
+  removeMemberValidator,
+  renameValidator,
+  sendAttachmentsValidator,
+  validateHandler,
+} from "../lib/validators.js";
 
 const chatRouter = express.Router();
 
 // After here user must be logged in to access the routes
 chatRouter.use(isAuthenticated);
-chatRouter.post("/new", newGroupChat);
+
+chatRouter.post("/new", newGroupValidator, validateHandler, newGroupChat);
 chatRouter.get("/my", getMyChats);
 chatRouter.get("/my/groups", getMyGroups);
-chatRouter.put("/addmembers", addMembers);
-chatRouter.put("/removemember", removeMembers);
-chatRouter.delete("/leave/:id", leaveGroup);
-chatRouter.post("/message", attachmentMulter, sendAttachments);
+chatRouter.put("/addmembers", addMemberValidator, validateHandler, addMembers);
+chatRouter.put(
+  "/removemember",
+  removeMemberValidator,
+  validateHandler,
+  removeMembers
+);
+chatRouter.delete("/leave/:id", chatIdValidator(), validateHandler, leaveGroup);
+chatRouter.post(
+  "/message",
+  attachmentMulter,
+  sendAttachmentsValidator(),
+  validateHandler,
+  sendAttachments
+);
 
 // Get Messages
-chatRouter.get("/message/:id", getMessages);
+chatRouter.get("/message/:id", chatIdValidator(), validateHandler, getMessages);
 
 // Get Chat Details, rename,delete
 chatRouter
   .route("/:id")
-  .get(getChatDetails)
-  .put(renameGroup)
-  .delete(deleteChat);
+  .get(chatIdValidator(), validateHandler, getChatDetails)
+  .put(renameValidator(), validateHandler, renameGroup)
+  .delete(chatIdValidator(), validateHandler, deleteChat);
 
 export default chatRouter;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,8 @@
 import express from "express";
 import {
+  acceptFriendRequest,
+  getMyFriends,
+  getMyNotifications,
   getMyProfile,
   login,
   logout,
@@ -8,16 +11,45 @@ import {
 } from "../controllers/user.controller.js";
 import { singleAvatar } from "../middlewares/multer.js";
 import { isAuthenticated } from "../middlewares/auth.js";
+import {
+  acceptRequestValidator,
+  loginValidator,
+  registerValidator,
+  sendRequestValidator,
+  validateHandler,
+} from "../lib/validators.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/new", singleAvatar, newUser);
-userRouter.post("/login", login);
+userRouter.post(
+  "/new",
+  singleAvatar,
+  registerValidator(),
+  validateHandler,
+  newUser
+);
+userRouter.post("/login", loginValidator(), validateHandler, login);
 
 // After here user must be logged in to access the routes
 userRouter.use(isAuthenticated);
+
 userRouter.get("/me", getMyProfile);
 userRouter.get("/logout", logout);
 userRouter.get("/search", searchUser);
 
+userRouter.put(
+  "sendrequest",
+  sendRequestValidator(),
+  validateHandler,
+  acceptFriendRequest
+);
+userRouter.put(
+  "/acceptrequest",
+  acceptRequestValidator(),
+  validateHandler,
+  acceptFriendRequest
+);
+
+userRouter.get("/notifications", getMyNotifications);
+userRouter.get("/friends", getMyFriends);
 export default userRouter;
