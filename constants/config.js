376eecc484a57fb44a87f7f02627f2b049df4c63
md-suhag <abdussalamsuhag104@gmail.com>
2024-12-17 22:48:17 +0600

    fixed cors issue

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "http://localhost:4173",
+    "https://my-chat-mern.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
